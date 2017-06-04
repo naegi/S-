38c604c5fafe3f0bbe248e2d61d6a5f76140260b
naegi <gouge.tristan@gmail.com>
2017-06-04 17:36:07 +0200

    add auto completion

--- src/Main.hs
+++ src/Main.hs
@@ -1,48 +1,57 @@
-import System.Console.Readline as R
+import System.Console.Haskeline as H
+import Data.List (isPrefixOf)
 import Control.Monad.Except
-import Control.Monad.State
+import Control.Monad.State.Strict
 import Control.Monad.Morph (hoist)
 import Control.Monad.Identity (Identity(..))
-import Data.Map as M
+import Data.Map as Map (keys, insert, unions)
 
 import Vals
 import Parse
 import Eval
 import DefaultEnv
 
-unwrapIO = hoist (hoist generalize)
-
-generalize :: Monad m => Identity a -> m a
-generalize = return . runIdentity
-
 interpret ::  String -> M SVal
 interpret str = parseS "" str >>= eval
 
+runEM = runStateT . runExceptT
 
-repl :: E (StateT Env IO) ()
+defaultCompl :: [String]
+defaultCompl = ["'("]
+
+findCompletion :: String -> StateT Env IO [Completion]
+findCompletion s = do 
+    (Env gbls lcs) <- get
+    return $ Prelude.map simpleCompletion $ filter (isPrefixOf s) (Map.keys (Map.unions (gbls:lcs)) ++ defaultCompl)
+
+settings :: Settings (StateT Env IO)
+settings = setComplete (H.completeWord Nothing " \t" findCompletion) H.defaultSettings
+
+repl :: InputT (StateT Env IO)()
 repl = do
-    maybeLine <- liftIO $ R.readline "S~> "
-    case maybeLine of 
-        Nothing     -> return () -- EOF / control-d
-        Just "exit" -> return ()
-        Just line -> do
-                liftIO $ R.addHistory line
-                val <- unwrapIO (interpret line)
-                        `catchError` report
-                liftIO $ printifnotnone val
-                unwrapIO $ ans val
+        maybeLine <- H.getInputLine "S~> "
+        case maybeLine of 
+            Nothing     -> return () -- EOF / control-d
+            Just "exit" -> return ()
+            Just line -> do
+                env <- lift get
+                let (val, env) = runIdentity $ runEM (interpret line) env
+                lift $ put env
+                val <- either report printifnotnone val
+                ans val
                 repl
 
-printifnotnone :: SVal -> IO ()
-printifnotnone SValNone = return ()
-printifnotnone a = print a 
+printifnotnone :: SVal -> InputT (StateT Env IO) SVal
+printifnotnone SValNone = return SValNone
+printifnotnone a = H.outputStrLn (show a) >> return a
 
-ans :: SVal -> M ()
+ans :: SVal -> InputT (StateT Env IO) ()
 ans SValNone = return ()
-ans a = setVar True "ans" a
+ans a = do 
+    (Env gbls lcs) <- lift get
+    lift $ put $ Env (Map.insert "ans" a gbls) lcs
 
-report :: SError -> E (StateT Env IO) SVal
-report err = liftIO (print err) >> return SValNone
+report :: SError -> InputT (StateT Env IO) SVal
+report err = H.outputStrLn (show err) >> return SValNone
 
-main :: IO ()
-main = void $ runStateT (runExceptT repl) makeEnv
+main = runStateT (runInputT settings repl) makeEnv
